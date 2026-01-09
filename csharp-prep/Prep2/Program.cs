0681c86721fef4d2375eb04b0ed935be59b07f68
Yin He <yheus25@gmail.com>
2026-01-09 12:28:32 -0700

    W01 Prove: Developer—C# Programming Parts 1–2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Please enter your grade: ");
+        string grade_str = Console.ReadLine();
+        int grade = int.Parse(grade_str);
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}");
+        
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass. Better luck next time!");
+        }
     }
 }
