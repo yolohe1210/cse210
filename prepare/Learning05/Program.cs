2dcabf09268ba787d11e1d31cface6805484b48f
Yin He <yheus25@gmail.com>
2026-03-03 11:32:59 -0700

    W09 Prepare: Learning Activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Blue", 2);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Black", 4, 6);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Purple", 3);
+        shapes.Add(s3);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
